e2eb5b9e852ff07a425ce9f4e602f94a79db3fd2
paskn <pashakhin@gmail.com>
2025-03-10 20:31:51 +0100

    Add missing resources for Spacy

--- install_coreferee_ru.sh
+++ install_coreferee_ru.sh
@@ -7,6 +7,9 @@ uv sync --no-cache
 # activate the environment
 source .venv/bin/activate
 
+# install one of spacy's models for Russian
+python -m spacy download ru_core_news_sm
+
 # test that spacy is installed and working
 python -m spacy validate
 
